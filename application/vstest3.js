55fa32314cb634f325286dfe0ca403cc0100d3e7
tomxucnxa <170015@qq.com>
2018-12-26 21:25:45 +0800

    Add vstest3 for wallet and gateway.

---
+++ application/vstest3.js
@@ -0,0 +1,69 @@
+/**
+  * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * This is an example based on fabric-sdk-node, it refers content of:
+ * https://fabric-sdk-node.github.io/master/index.html
+ * https://github.com/hyperledger/fabric-sdk-node
+ * https://fabric-sdk-node.github.io/master/tutorial-network-config.html
+ * 
+ * This program uses config2.json, what is a common connection profile.
+ * It will utilze FileSystemWallet and Gateway, what is from fabric-network module.
+ */
+
+'use strict';
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+const winston = require('winston');
+const {Gateway, FileSystemWallet, X509WalletMixin} = require('fabric-network');
+
+var logger = new (winston.Logger)({transports: [new (winston.transports.Console)()]});
+
+// Call the only test function.
+test();
+
+
+async function test() {
+    const identityLabel = 'Admin@org1.example.com';
+    const wallet = await initAdminWallet(identityLabel);
+    const gateway = new Gateway();
+
+    await gateway.connect(path.join(__dirname, './config2.json'),
+        {
+            wallet: wallet,
+            identity: identityLabel
+        });
+
+    logger.info('Gateway connects get succeed.');
+
+    const network = await gateway.getNetwork('mychannel');
+    const contract = await network.getContract('vehiclesharing');
+    const result = await contract.executeTransaction('findVehicle', 'C123');
+    gateway.disconnect();
+    
+    logger.info('Result', Buffer.from(result).toString());
+}
+
+async function initAdminWallet(identityLabel) {
+    // Hardcode crypto materials of Admin@org1.example.com.
+    const keyPath = path.join(__dirname, "../../fabric-samples/first-network/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore/ae5b21ead4fa0954915a215b82f5ddc18dac94680c169ae577384d4b4ef89300_sk");
+    const keyPEM = Buffer.from(fs.readFileSync(keyPath)).toString();
+    const certPath = path.join(__dirname, "../../fabric-samples/first-network/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/signcerts/Admin@org1.example.com-cert.pem");
+    const certPEM = Buffer.from(fs.readFileSync(certPath)).toString();
+
+    const mspId = 'Org1MSP';
+    const identity = X509WalletMixin.createIdentity(mspId, certPEM, keyPEM)
+
+    const wallet = new FileSystemWallet('/tmp/wallet/test1');
+    await wallet.import(identityLabel, identity);
+
+    if (await wallet.exists(identityLabel)) {
+        logger.info('Identity %s exists.', identityLabel);
+    }
+    else {
+        logger.error('Identity %s does not exist.', identityLabel);
+    }
+    return wallet;
+}
